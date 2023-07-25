f9a345f7c14e9d7b6e596658a930e39a0f63c019
juliidiaz <juliana.diaz@live.com>
2023-07-25 20:13:49 -0300

    ultimo

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const app = express();
 
 const port =process.env.PORT || 3001;
 app.listen(port, () =>console.log('Servidor corriendo en el puerto ${port}'));
-app.use (express.static("public"))
+app.use (express.static("public"));
 app.get("/", (req, res)=> {console.log(__dirname);
 res.sendFile(path.resolve(__dirname, "./views/index.html"))});
