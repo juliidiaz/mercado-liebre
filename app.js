1848530c123fa0c74f96d8d97aff464cd694f4d9
juliidiaz <juliana.diaz@live.com>
2023-07-25 11:12:21 -0300

    puertos

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require ("path");
 const app = express();
 
 
-app.listen (3030, ()=>console.log("Servidor andando"));
+const port =process.env.PORT || 3001;
+app.listen(port, () =>console.log('Servidor corriendo en el puerto ${port}'));
 app.use (express.static("public"))
 app.get("/", (req, res)=> {console.log(__dirname);
 res.sendFile(path.resolve(__dirname, "./views/index.html"))});
