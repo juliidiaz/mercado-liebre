04507a215de8c619a6d313d981bda9cb8f2380d4
juliidiaz <juliana.diaz@live.com>
2023-07-27 11:41:13 -0300

    Con los GET activos

--- app.js
+++ app.js
@@ -6,5 +6,17 @@ const app = express();
 const port =process.env.PORT || 3001;
 app.listen(port, () =>console.log('Servidor corriendo en el puerto ${port}'));
 app.use (express.static("public"));
-app.get("/", (req, res)=> {console.log(__dirname);
-res.sendFile(path.resolve(__dirname, "./views/index.html"))});
+
+app.get("/", (req, res)=> {
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))});
+
+app.get("/views/register.html", (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/views/loguin.html", (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, "./views/loguin.html"))
+});
