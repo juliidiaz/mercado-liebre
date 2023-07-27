3b7a0a48666209c955f0952482e993ca2f33dacb
juliidiaz <juliana.diaz@live.com>
2023-07-27 20:41:19 -0300

    listo

--- app.js
+++ app.js
@@ -11,12 +11,20 @@ app.get("/", (req, res)=> {
     console.log(__dirname);
     res.sendFile(path.resolve(__dirname, "./views/index.html"))});
 
-app.get("/views/register.html", (req, res) => {
+app.get("/register", (req, res) => {
     console.log(__dirname);
     res.sendFile(path.resolve(__dirname, "./views/register.html"))
 });
 
-app.get("/views/loguin.html", (req, res) => {
+app.get("/loguin", (req, res) => {
     console.log(__dirname);
     res.sendFile(path.resolve(__dirname, "./views/loguin.html"))
+});
+
+app.post("/loguin", (req, res) => {
+    res.redirect("/");
+});
+
+app.post("/register", (req, res) => {
+    res.redirect("/");
 });
